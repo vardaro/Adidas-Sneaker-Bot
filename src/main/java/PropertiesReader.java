266489b0f31fe256437b189348c74d7744041b9e
vardaro <anthonyavardaro@yahoo.com>
2017-04-19 17:51:08 -0500

    its ugly im warning u

---
+++ src/main/java/PropertiesReader.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created by justa on 4/18/2017.
+ */
+public class PropertiesReader {
+  Properties properties = new Properties();
+  String propertiesPath;
+
+  public PropertiesReader(String propertiesPath) {
+    this.propertiesPath = propertiesPath;
+  }
+
+  /**
+   * Returns the value of the requested key from the properties path
+   *
+   * @param key key of value requested
+   * @return value of key
+   */
+  public String get(String key) {
+    try {
+      InputStream input = getClass().getClassLoader().getResourceAsStream(propertiesPath);
+      properties.load(input);
+      return properties.getProperty(key);
+    } catch (IOException io) {
+      System.out.println("Error getting property " + key + "\nException: " + io);
+      return null;
+    }
+  }
+}
